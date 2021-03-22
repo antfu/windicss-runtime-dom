f9f6ae4742f3b670f533f3e046d6846b34b2f243
Anthony Fu <anthonyfu117@hotmail.com>
2021-03-23 03:29:21 +0800

    chore: build to d.ts

--- rollup.config.js
+++ rollup.config.js
@@ -2,26 +2,39 @@ import typescript from 'rollup-plugin-typescript2'
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import { terser } from 'rollup-plugin-terser'
+import dts from 'rollup-plugin-dts'
 
-export default {
-  input: 'src/index.ts',
-  output: [
-    { file: 'dist/index.js', format: 'iife' },
-    {
-      file: 'dist/index.min.js',
-      format: 'iife',
-      plugins: [
-        terser({
-          format: {
-            comments: false,
-          },
-        }),
-      ],
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      { file: 'dist/index.js', format: 'iife' },
+      {
+        file: 'dist/index.min.js',
+        format: 'iife',
+        plugins: [
+          terser({
+            format: {
+              comments: false,
+            },
+          }),
+        ],
+      },
+    ],
+    plugins: [
+      typescript({ useTsconfigDeclarationDir: true }),
+      commonjs(),
+      resolve(),
+    ],
+  },
+  {
+    input: 'src/index.ts',
+    output: {
+      file: 'dist/index.d.ts',
+      format: 'es',
     },
-  ],
-  plugins: [
-    typescript({ useTsconfigDeclarationDir: true }),
-    commonjs(),
-    resolve(),
-  ],
-}
+    plugins: [
+      dts(),
+    ],
+  },
+]
